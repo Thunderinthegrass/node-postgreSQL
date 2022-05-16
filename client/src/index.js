0b19a3c083ee42e92d02fe064decff8554fcbf4d
ThunderInTheGrass <propeller8924@gmail.com>
2022-05-17 00:05:04 +0800

    Mobx

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {createContext} from 'react';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
+import UserStore from './store/UserStore';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+export const Context = createContext(null);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(
+    <Context.Provider value={{
+      user: new UserStore()
+    }}>
+      <App />
+    </Context.Provider>,
+  document.getElementById('root')
 );
